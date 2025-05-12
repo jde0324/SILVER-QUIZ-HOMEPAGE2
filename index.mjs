a4ddd942d4f92524b76f63fe3d10117c84d09031
BANNYJULY <jde0324@naver.com>
2025-05-12 14:03:35 +0900

    Test Submit_LEE DONG JU
    최종본 제출입니다.

--- index.mjs
+++ index.mjs
@@ -5,13 +5,51 @@ const getImageBtn = document.getElementById("get-image-btn");
 const workbookModal = document.getElementById("workbook-modal");
 const workbookModalInner = document.getElementById("workbook-modal-inner");
 const workbookModalCloseBtn = document.getElementById("workbook-modal-close-btn");
+const prize = document.getElementById("prize");
 
 workbookModalCloseBtn.addEventListener("click", closeModal);
 function closeModal() {
   workbookModal.style.display = "none";
 }
 
+// Btn.addEventListner("click", function() {
+//   workbookModal.classList.add('on');
+// });
+
 getImageBtn.addEventListener("click", function() {
   getImageBtn.style.background = "rgb(218, 52, 2)";
+   workbookModal.classList.add('on');
 });
 
+Btn.addEventListener("click", function() {
+  Btn.style.background = "rgb(105, 39, 180)";
+});
+
+function clickresult() {
+  let score = 0;
+  const toTalscore = 20;
+
+  const answers = {
+    book: "biography-story",
+    // card: "remember-card",
+    // quiz: "proverb-quiz",
+    // coloring: "animal-coloring",
+    puzzle: "wedding-puzzle"
+  };
+
+  let totalCount = 0;
+
+  for (let question in answers) {
+    const selected = document.querySelector(input[name="${question}"]:checked);
+    if (selected && selected.value === answers[question]) {
+      totalCount++;
+    }
+  }
+
+  score = totalCount * toTalscore;
+
+  document.getElementById("score").innerText =
+    `점수: ${score}점 
+    (정답 ${totalCount}개 
+    / 5문제)`;
+}
