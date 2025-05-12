f5f2797b8c8f9b4dee84680360b384e6cef05ed7
BANNYJULY <jde0324@naver.com>
2025-05-12 12:55:50 +0900

    Test2_LDJ
    최종본 제출합니다.

--- index.mjs
+++ index.mjs
@@ -2,7 +2,6 @@ import { booksData } from "./data.js";
 
 const workbookRadios = document.getElementById("workbook-radios");
 const getImageBtn = document.getElementById("get-image-btn");
-const gifsOnlyOption = document.getElementById("gifs-only-option");
 const workbookModal = document.getElementById("workbook-modal");
 const workbookModalInner = document.getElementById("workbook-modal-inner");
 const workbookModalCloseBtn = document.getElementById("workbook-modal-close-btn");
@@ -12,84 +11,7 @@ function closeModal() {
   workbookModal.style.display = "none";
 }
 
-getImageBtn.addEventListener("click", renderBook);
+getImageBtn.addEventListener("click", function() {
+  getImageBtn.style.background = "rgb(218, 52, 2)";
+});
 
-workbookRadios.addEventListener("change", function (e) {
-    const radios = document.getElementsByClassName("radio");
-    for (let radio of radios) {
-      radio.classList.remove("highlight");
-    }
-    const selectedElement = document.getElementById(e.target.id);
-    const selectedParentEl = selectedElement.parentElement;
-    selectedParentEl.classList.add("highlight");
-  });
-
-  function renderBook() {
-    const bookObject = getSinglebookObject();
-    workbookModalInner.innerHTML = `
-    <img
-    class="book-img"
-    src="./images/${bookObject.image}"
-    >
-    `;
-    memeModal.style.display = "flex";
-  }
-  
-  function getSingleBookObject() {
-    const booksArray = getMatchingBooksArray();
-    if (booksArray.length === 1) {
-      return booksArray[0];
-    } else {
-      const randomNumber = Math.floor(Math.random() * booksArray.length);
-      return booksArray[randomNumber];
-    }
-  }
-
-  function getMatchingBooksArray() {
-    const isGif = gifsOnlyOption.checked;
-    const selectedworkbook = document.querySelector(
-      `input[type="radio"]:checked`
-    ).value;
-    const matchingBooksArray = booksData.filter(function (book) {
-      if (isGif) {
-        return book.workbook.Tags.includes(selectedworkbook) && book.isGif;
-      } else {
-        return book.workTags.includes(selectedworkbook);
-      }
-    });
-    return matchingBooksArray;
-  }
-
-  function getWorkbooksArray(books) {
-    const workbooksArray = [];
-    for (let item of cats) {
-      for (let workbook of item.workbookTags) {
-        if (!workbooksArray.includes(book)) {
-          workbooksArray.push(book);
-        }
-      }
-    }
-    return workbookArray;
-  }
-
-  function renderWorkbookRadios(books) {
-    const workbooks = getWorkbooksArray(books);
-    let radioItems = "";
-    for (let workbook of workbooks) {
-      radioItems += `
-      <div class="radio">
-          <label for="${workbook}">${workbook}</label>
-          <input 
-              type="radio"
-              id="${workbook}"
-              value="${workbook}"
-              name="workbooks"
-          >
-      </div>
-      `;
-    }
-    workbookRadios.innerHTML = radioItems;
-  }
-  
-  renderWorkbookRadios(booksData);
-  
